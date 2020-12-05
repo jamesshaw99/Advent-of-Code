2929aa09f101a3210d7b9377cdc864339a823576
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2020-12-05 17:42:06 +0000

    2019 day 7 (not complete)

--- 2019/src/day7.java
+++ 2019/src/day7.java
@@ -1,12 +1,13 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class day7 {
     public static void main(String[] args) {
         List<Integer> dataList = new ArrayList<Integer>();
-        File file = new File("data.txt");
+        File file = new File("inputs/day7test.txt");
         BufferedReader reader = null;
 
         try {
@@ -29,9 +30,46 @@ public class day7 {
             }
         }
 
-        List<Integer> list = new ArrayList<Integer>(dataList);
-        List<Integer> list2 = new ArrayList<Integer>(dataList);
+        List<List<Integer>> inputs = new ArrayList<>();
 
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++){
+                if (j != i) {
+                    for (int k = 0; k < 5; k++) {
+                        if (k != i && k != j) {
+                            for (int l = 0; l < 5; l++) {
+                                if(l != i && l != j && l !=k) {
+                                    for (int m = 0; m < 5; m++){
+                                        if(m != i && m != j && m != k && m != l) {
+                                            inputs.add(Arrays.asList(i,j,k,l,m));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        List<Integer> output = new ArrayList<Integer>();
+        List<Integer> finalOutput = new ArrayList<Integer>();
+        for (List<Integer> phaseSettings: inputs){
+            int input = 0;
+            for (int i = 0; i < 5; i++){
+                List<Integer> result = new ArrayList<Integer>(doIntcode(dataList, phaseSettings.get(i), input));
+                System.out.println(result);
+                input = result.get(0);
+                output.add(input);
+            }
+            finalOutput.add(Collections.max(output));
+        }
+        System.out.println(Collections.max(finalOutput));
+
+
+    }
+
+    public static List<Integer> doIntcode(List<Integer> list, int input1, int input2){
+        List<Integer> outputs = new ArrayList<Integer>();
         int i = 0;
         while (i < list.size()) {
             int opcode = list.get(i) % 100;
@@ -56,12 +94,19 @@ public class day7 {
                     i++;
                 }
             } else if(opcode == 3) {
-                Scanner num = new Scanner(System.in);
-                System.out.print("Please provide the ID of the system to test: ");
-                list.set(list.get(i+1), num.nextInt());
+                //Scanner num = new Scanner(System.in);
+                System.out.print("Inputs: ");
+                if (input1 != -1){
+                    list.set(list.get(i+1), input1);
+                    System.out.println(input1);
+                    input1 = -1;
+                } else {
+                    list.set(list.get(i+1), input2);
+                    System.out.println(input2);
+                }
                 i += 2;
             } else if(opcode == 4) {
-                System.out.println(list.get(param1Mode == 0 ? list.get((i+1)) : i+1));
+                outputs.add(list.get(param1Mode == 0 ? list.get((i+1)) : i+1));
                 i += 2;
             } else if(opcode == 5) {
                 if(list.get(param1Mode == 0 ? list.get((i+1)) : i+1) != 0){
@@ -89,9 +134,13 @@ public class day7 {
                     list.set(list.get(i+3), 0);
                 }
                 i += 4;
-            } else if(opcode == 99){
+            } else {
+                if(outputs.size() == 0) {
+                    outputs.add(0);
+                }
                 break;
             }
         }
+        return outputs;
     }
 }
